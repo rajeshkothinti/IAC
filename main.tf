784bf44af133f522eb75d93adcadb717ae2e20a6
kothinti9 <74715112+kothinti9@users.noreply.github.com>
2021-03-30 15:34:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+terraform {
+  
+  required_version = "0.14.7"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.30.0"
+      }
+    }
+  }
 
+provider "aws" {
+  region = var.aws_region
+  }
