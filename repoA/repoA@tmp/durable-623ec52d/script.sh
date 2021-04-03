431fcc4b9ced9c689f8da834b8ccff30f205ee05
kRajr <42643252+kRajr@users.noreply.github.com>
2021-04-03 07:57:30 +0530

    Delete repoA directory

--- repoA/repoA@tmp/durable-623ec52d/script.sh
+++
@@ -1,7 +0,0 @@
-
-    git remote set-url origin git@github.com:kRajr/IAC.git
-    git add  -A .
-    git stash
-    git pull git@github.com:kRajr/IAC.git test
-    git commit -m "abc"
-    git push git@github.com:kRajr/IAC.git test 
